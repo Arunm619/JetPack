e42b2993d9724d9b476815e621a7c57de31d6aaa
Arun sudharsan <sudarun@amazon.com>
2020-05-07 16:20:53 +0530

    Added a basic user functionality where user can type a title in edit text and upon hitting the submit button the title will be set on the text view.

--- app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
@@ -1,11 +1,37 @@
 package io.arunbuilds.jetpack
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var titleText: TextView
+    lateinit var titleEditText: EditText
+    lateinit var submitButton: Button
+    lateinit var parentView: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initViews()
+        submitButton.setOnClickListener {
+            if (titleEditText.text.isNullOrEmpty()) {
+                Snackbar.make(parentView, "Please enter a valid title", Snackbar.LENGTH_SHORT)
+                    .show()
+                return@setOnClickListener
+            }
+            titleText.text = titleEditText.text
+        }
+    }
+
+    private fun initViews() {
+        titleText = findViewById(R.id.title_text)
+        titleEditText = findViewById(R.id.title_edit_text)
+        submitButton = findViewById(R.id.submit_button)
+        parentView = findViewById(android.R.id.content)
     }
 }
