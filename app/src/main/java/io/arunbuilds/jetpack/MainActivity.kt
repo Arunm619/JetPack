92517e96c9ddb819e71d0247c672305ed8ffe88b
Arun sudharsan <sudarun@amazon.com>
2020-05-07 16:49:28 +0530

    added new object and binded with view

--- app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
@@ -1,31 +1,29 @@
 package io.arunbuilds.jetpack
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.android.material.snackbar.Snackbar
 import io.arunbuilds.jetpack.databinding.ActivityMainBinding
+import io.arunbuilds.jetpack.model.Person
+import io.arunbuilds.jetpack.model.getRandomPerson
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.submitButton.setOnClickListener {
+            val student = getRandomPerson()
+            bindPersonIntoViews(student)
+        }
+    }
+
+    private fun bindPersonIntoViews(student: Person) {
         binding.apply {
-            submitButton.setOnClickListener {
-                if (titleEditText.text.isNullOrEmpty()) {
-                    Snackbar.make(binding.root, "Please enter a valid title", Snackbar.LENGTH_SHORT)
-                        .show()
-                    return@setOnClickListener
-                }
-                titleText.text = titleEditText.text
-            }
+            nameTextView.text = student.name
+            ageTextView.text = student.age.toString()
         }
     }
 
