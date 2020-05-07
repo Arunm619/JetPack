43c1b388aa1ac243ef07261192f0103c2c80f85c
Arun sudharsan <sudarun@amazon.com>
2020-05-07 19:42:01 +0530

    Reset Code for Starting Nav graph

--- app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
@@ -10,15 +10,10 @@ import io.arunbuilds.jetpack.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var viewModel: MainActivityViewModel
-    private lateinit var viewModelFactory: MainActivityViewModelFactory
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewModelFactory = MainActivityViewModelFactory(100)
-        viewModel = ViewModelProvider(this, viewModelFactory).get(MainActivityViewModel::class.java)
-        binding.lifecycleOwner = this
-        binding.myViewModel = viewModel
     }
 
 }
