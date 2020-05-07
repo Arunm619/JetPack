a3b7a76eb7b6e1e9210317a9ff0cfbc341b4a6cc
Arun sudharsan <sudarun@amazon.com>
2020-05-07 16:28:00 +0530

    Added Databinding library to remove findviewbyid calls

--- app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
+++ app/src/main/java/io/arunbuilds/jetpack/MainActivity.kt
@@ -6,32 +6,27 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.google.android.material.snackbar.Snackbar
+import io.arunbuilds.jetpack.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var titleText: TextView
-    lateinit var titleEditText: EditText
-    lateinit var submitButton: Button
-    lateinit var parentView: View
+
+    private lateinit var binding : ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        initViews()
-        submitButton.setOnClickListener {
-            if (titleEditText.text.isNullOrEmpty()) {
-                Snackbar.make(parentView, "Please enter a valid title", Snackbar.LENGTH_SHORT)
-                    .show()
-                return@setOnClickListener
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.apply {
+            submitButton.setOnClickListener {
+                if (titleEditText.text.isNullOrEmpty()) {
+                    Snackbar.make(binding.root, "Please enter a valid title", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnClickListener
+                }
+                titleText.text = titleEditText.text
             }
-            titleText.text = titleEditText.text
         }
     }
 
-    private fun initViews() {
-        titleText = findViewById(R.id.title_text)
-        titleEditText = findViewById(R.id.title_edit_text)
-        submitButton = findViewById(R.id.submit_button)
-        parentView = findViewById(android.R.id.content)
-    }
 }
